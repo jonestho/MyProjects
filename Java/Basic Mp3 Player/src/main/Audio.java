f05e23ad29d0c2412b9d4a6fa0cb32e5da2704eb
thomas <jonestho@mail.gvsu.edu>
2022-12-22 15:28:46 -0500

    initializes java files

---
+++ Java/Basic Mp3 Player/src/main/Audio.java
@@ -0,0 +1,6 @@
+package main;
+
+public class Audio {
+    public void playAudio(){}
+    public void pauseAudio(){}
+}
---
+++ Java/Basic Mp3 Player/src/main/Mp3Player.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.*;
+
+public class Mp3Player{
+    public static void main(String[] args){
+        JFrame mp3Frame = new JFrame("Basic Mp3 Player");
+
+        mp3Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mp3Frame.setSize(900, 600);
+
+        mp3Frame.setVisible(true);
+    }
+}
